08f4f64670a7583a6455dfc917669047bc26c885
LGKaufmann <lautykaufmann76@gmail.com>
2024-09-08 19:23:00 -0300

    for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://spa-project-back.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3001/";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
