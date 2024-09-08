14448c4f1567920e854b4ad7f9e00b9d77f8a203
LGKaufmann <lautykaufmann76@gmail.com>
2024-09-08 19:25:40 -0300

    for deploy 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
+import axios2 from "../utils/axios.ts";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://spa-project-back.onrender.com";
+axios2.defaults.baseURL = "https://spa-project-back.onrender.com";
 // axios.defaults.baseURL = "http://localhost:3001/";
 
 createRoot(document.getElementById("root")).render(
