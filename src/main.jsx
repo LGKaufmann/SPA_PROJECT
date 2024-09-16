5f201fe35719b385f19ef07a029325f8afc1afce
unknown <lautykaufmann76@gmail.com>
2024-09-16 14:47:29 -0300

    change for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,10 @@ import { PersistGate } from "redux-persist/integration/react";
 import axios2 from "../utils/axios.ts";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://spa-project-back.onrender.com/";
-// axios2.defaults.baseURL = "https://spa-project-back.onrender.com/";
-axios.defaults.baseURL = "http://localhost:3001/";
-axios2.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://spa-project-back.onrender.com/";
+axios2.defaults.baseURL = "https://spa-project-back.onrender.com/";
+// axios.defaults.baseURL = "http://localhost:3001/";
+// axios2.defaults.baseURL = "http://localhost:3001/";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
