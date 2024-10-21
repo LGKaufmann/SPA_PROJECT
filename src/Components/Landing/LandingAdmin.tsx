a016df3dfa8ad6e213cf6a70f6dc0b3b76a2b9d6
unknown <lautykaufmann76@gmail.com>
2024-10-21 17:47:02 -0300

    for deploy

--- src/Components/Landing/LandingAdmin.tsx
+++ src/Components/Landing/LandingAdmin.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Whatsapp from "../../../public/assets/WhatsApp.svg";
+import Whatsapp from "/assets/WhatsApp.svg";
 import SidebarAdmin from "./SidebarAdmin";
 
 interface ITurno {
